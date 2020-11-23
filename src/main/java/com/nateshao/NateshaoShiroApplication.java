a5ecccc9111ef01561776707d0a46fe572fab735
nateshao <shaotongjie@gmail.com>
2020-11-24 00:05:31 +0800

    初始化完成

---
+++ src/main/java/com/nateshao/NateshaoShiroApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoShiroApplication.class, args);
+    }
+
+}
