e50bf674f9d505c2e42fdbc0d306d88c9aaf7124
nateshao <shaotongjie@gmail.com>
2020-11-24 15:26:50 +0800

    初始化完成

--- src/main/java/com/nateshao/admin/controller/LoginController.java
+++ src/main/java/com/nateshao/admin/controller/LoginController.java
@@ -8,5 +8,167 @@ package com.nateshao.admin.controller;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
-public class LoginController {
+
+@Controller
+public class LonginController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LonginController.class);
+
+    public final static String LOGIN_TYPE = "loginType";
+
+    @Autowired
+    @Qualifier("captchaProducer")
+    DefaultKaptcha captchaProducer;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MenuService menuService;
+
+    public enum LoginTypeEnum {
+        PAGE,ADMIN;
+    }
+
+    @GetMapping(value = "")
+    public String welcome() {
+        return "redirect:admin";
+    }
+
+    @GetMapping(value = {"admin","admin/index"})
+    public String adminIndex(RedirectAttributes attributes, ModelMap map) {
+        Subject s = SecurityUtils.getSubject();
+        attributes.addFlashAttribute(LOGIN_TYPE, LoginTypeEnum.ADMIN);
+        if(s.isAuthenticated()) {
+            return "redirect:index";
+        }
+        return "redirect:toLogin";
+    }
+
+    @GetMapping(value = "toLogin")
+    public String adminToLogin(HttpSession session, @ModelAttribute(LOGIN_TYPE) String loginType) {
+        if(StringUtils.isBlank(loginType)) {
+            LoginTypeEnum attribute = (LoginTypeEnum) session.getAttribute(LOGIN_TYPE);
+            loginType = attribute == null ? loginType : attribute.name();
+        }
+
+        if(LoginTypeEnum.ADMIN.name().equals(loginType)) {
+            session.setAttribute(LOGIN_TYPE, LoginTypeEnum.ADMIN);
+            return "admin/login";
+        }else {
+            session.setAttribute(LOGIN_TYPE, LoginTypeEnum.PAGE);
+            return "login";
+        }
+    }
+
+    @GetMapping(value = "index")
+    public String index(HttpSession session, @ModelAttribute(LOGIN_TYPE) String loginType) {
+        if(StringUtils.isBlank(loginType)) {
+            LoginTypeEnum attribute = (LoginTypeEnum) session.getAttribute(LOGIN_TYPE);
+            loginType = attribute == null ? loginType : attribute.name();
+        }
+        if(LoginTypeEnum.ADMIN.name().equals(loginType)) {
+            AuthRealm.ShiroUser principal = (AuthRealm.ShiroUser) SecurityUtils.getSubject().getPrincipal();
+            session.setAttribute("icon",StringUtils.isBlank(principal.getIcon()) ? "/static/admin/img/face.jpg" : principal.getIcon());
+            return "admin/index";
+        }else {
+            return "index";
+        }
+
+    }
+
+    @GetMapping("/getCaptcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //设置页面不缓存
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        String verifyCode = captchaProducer.createText();
+        //将验证码放到HttpSession里面
+        request.getSession().setAttribute(Constants.VALIDATE_CODE, verifyCode);
+        LOGGER.info("本次生成的验证码为[" + verifyCode + "],已存放到HttpSession中");
+        //设置输出的内容的类型为JPEG图像
+        response.setContentType("image/jpeg");
+        BufferedImage bufferedImage = captchaProducer.createImage(verifyCode);
+        //写给浏览器
+        ImageIO.write(bufferedImage, "JPEG", response.getOutputStream());
+    }
+
+    @PostMapping("admin/login")
+    @SysLog("用户登录")
+    @ResponseBody
+    public ResponseEntity adminLogin(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String rememberMe = request.getParameter("rememberMe");
+        String code = request.getParameter("code");
+        if(StringUtils.isBlank(username) || StringUtils.isBlank(password)){
+            return ResponseEntity.failure("用户名或者密码不能为空");
+        }else if(StringUtils.isBlank(code)){
+            return ResponseEntity.failure("验证码不能为空");
+        }
+        HttpSession session = request.getSession();
+        if(session == null){
+            return ResponseEntity.failure("session超时");
+        }
+        String trueCode = (String)session.getAttribute(Constants.VALIDATE_CODE);
+        if(StringUtils.isBlank(trueCode)){
+            return ResponseEntity.failure("验证码超时");
+        }
+        if(StringUtils.isBlank(code) || !trueCode.toLowerCase().equals(code.toLowerCase())){
+            return ResponseEntity.failure("验证码错误");
+        }else {
+            /*当前用户*/
+            String errorMsg = null;
+            Subject user = SecurityUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken(username,password,Boolean.valueOf(rememberMe));
+            try {
+                user.login(token);
+                LOGGER.debug(username+"用户"+LocalDate.now().toString()+":======》登陆系统!");
+            }catch (IncorrectCredentialsException e) {
+                errorMsg = "用户名密码错误!";
+            }catch (UnknownAccountException e) {
+                errorMsg = "账户不存在!";
+            }catch (LockedAccountException e) {
+                errorMsg = "账户已被锁定!";
+            }catch (UserTypeAccountException e) {
+                errorMsg = "账户不是管理用户!";
+            }
+
+            if(StringUtils.isBlank(errorMsg)) {
+                ResponseEntity responseEntity = new ResponseEntity();
+                responseEntity.setSuccess(Boolean.TRUE);
+                responseEntity.setAny("url","index");
+                return responseEntity;
+            }else {
+                return ResponseEntity.failure(errorMsg);
+            }
+        }
+    }
+
+    @GetMapping("admin/main")
+    public String main(ModelMap map){
+        return "admin/main";
+    }
+
+    /***
+     * 获得用户所拥有的菜单列表
+     * @return
+     */
+    @GetMapping("/admin/user/getUserMenu")
+    @ResponseBody
+    public List<ShowMenuVo> getUserMenu(){
+        String userId = MySysUser.id();
+        List<ShowMenuVo> list = menuService.getShowMenuByUser(userId);
+        return list;
+    }
+
+    @GetMapping("systemLogout")
+    @SysLog("退出系统")
+    public String logOut(){
+        SecurityUtils.getSubject().logout();
+        return "redirect:admin";
+    }
+
 }
+
