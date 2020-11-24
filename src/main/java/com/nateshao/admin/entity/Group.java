c598e865e5894c1b04c1f87a569ddff9c208ff7c
nateshao <shaotongjie@gmail.com>
2020-11-24 15:36:00 +0800

    初始化完成

---
+++ src/main/java/com/nateshao/admin/entity/Group.java
@@ -0,0 +1,17 @@
+package com.nateshao.admin.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/24 15:35
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description：
+ */
+@TableName("sys_group")
+public class Group extends TreeEntity<Group> {
+    private static final long serialVersionUID = 1L;
+}
